d6a48f25c74427f9a41d3bf7bc8fed18b1e7fdef
jarredcsimon <jarredcsimon@gmail.com>
2021-03-22 08:23:08 -0500

    Done with user login and auth as well as checking if user is admin

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const express = require('express')
 const router = express.Router()
 
 //requireSign in will secure a route to only those signed in
-const { requireSignin } = require('../controllers/auth.js')
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth.js')
 
 
 const { userById } = require('../controllers/user.js')
@@ -12,7 +12,7 @@ const { userById } = require('../controllers/user.js')
 
 //routes
 
-router.get("/test/:userId",requireSignin, (req,res)=>{
+router.get("/test/:userId",requireSignin, isAuth, isAdmin, (req,res)=>{
     res.json({
         user: req.profile
     })
