ab6f05fb767998a786ed1ec3b29aee19546876da
jarredsimon <48073952+jarredsimon@users.noreply.github.com>
2021-03-23 13:01:29 -0500

    add functions to get all categories and wrapped up the back end

--- controllers/product.js
+++ controllers/product.js
@@ -11,7 +11,8 @@ exports.productById = (req, res, next, id)=>{
     Product.findById(id).exec((err,product)=>{
         if(err || !product){
             return res.status(404).json({
-                error:"Product not found"
+                error:"Product not found",
+                from: "productId"
             })
         }
         req.product = product
@@ -157,10 +158,115 @@ exports.list = (req,res)=>{
         .exec((err, products)=>{
             if (err){
                 return res.status(400).json({
-                    message:"Product not found"
+                    message:"Product not found",
+                    from: "list"
                 })
             }
-            res.send(products)
+            res.json(products)
         })
 
+}
+
+//show related items to the current list
+//It will find the product based on the request product category
+//other products that have the same category will be returned
+exports.listRelated = (req,res)=>{
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6
+
+    //$ne means not encluded, so we are finding all products except for the current product,
+    //then we are finding all products based on category
+    Product.find({_id:{$ne: req.product}, category: req.product.category})
+        .limit(limit)
+        .populate('category', '_id name')
+        .exec((err, products)=>{
+            if (err){
+                return res.status(400).json({
+                    message:"Products not found",
+                    from: "listRelated"
+                })
+            }
+            res.json(products)
+        })
+
+
+}
+
+
+//List all categories
+exports.listCategories = (req,res)=>{
+    //in the curly braces we can pass queries, for now the value is null
+    Product.distinct("category", {}, (err, categories)=>{
+        if (err){
+            return res.status(400).json({
+                "message": "Categories not Found"
+            })
+        }
+        res.json(categories)
+    })
+
+}
+
+
+/**
+ * list products by search
+ * we will implement product search in react frontend
+ * we will show categories in checkbox and price range in radio buttons
+ * as the user clicks on those checkbox and radio buttons
+ * we will make api request and show the products to users based on what he wants
+ */
+
+exports.listBySearch = (req, res) => {
+    let order = req.body.order ? req.body.order : "desc";
+    let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    let skip = parseInt(req.body.skip);
+    let findArgs = {};
+ 
+    // console.log(order, sortBy, limit, skip, req.body.filters);
+    // console.log("findArgs", findArgs);
+ 
+    for (let key in req.body.filters) {
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {
+                // gte -  greater than price [0-10]
+                // lte - less than
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1]
+                };
+            } else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    }
+ 
+    Product.find(findArgs)
+        .select("-photo")
+        .populate("category")
+        .sort([[sortBy, order]])
+        .skip(skip)
+        .limit(limit)
+        .exec((err, data) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Products not found"
+                });
+            }
+            res.json({
+                size: data.length,
+                data
+            });
+        });
+};
+
+//will return product photo then continue
+//works as middleware
+
+exports.photo=(req,res,next)=>{
+    if(req.product.photo.data){
+        //we have to set the content type since it isn't JSON
+        res.set('Content-Type', req.product.photo.contentType)
+        return res.send(req.product.photo.data)
+    }
+    next()
 }
--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 
-const { create, productById, read, remove, update, list } = require('../controllers/product.js')
+const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch, photo } = require('../controllers/product.js')
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth.js')
 const { userById } = require('../controllers/user.js')
 
@@ -15,6 +15,10 @@ router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create)
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove)
 router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update)
 router.get('/products',list)
+router.get('/products/related/:productId',listRelated)
+router.get('/products/categories', listCategories)
+router.post("/products/by/search", listBySearch);
+router.get('/product/photo/:productId', photo)
 
 
 router.param('userId', userById)
