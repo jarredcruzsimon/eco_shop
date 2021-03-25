017723737fd7f28b09703c5458731b729a2675e6
jarredcsimon <jarredcsimon@gmail.com>
2021-03-24 23:58:18 -0500

    admin can now create new category within the admin dashboard

--- ecommerce-front/src/admin/AddCategory.js
+++ ecommerce-front/src/admin/AddCategory.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Layout from '../core/Layout.js'
 import { isAuthenticated } from '../auth/index.js'
 import { Link } from 'react-router-dom'
+import { createCategory } from './apiAdmin'
 
 const AddCategory =()=>{
     
@@ -22,26 +23,65 @@ const clickSubmit=(e)=>{
     e.preventDefault()
     setError('')
     setSuccess(false)
-
     //make request to api to create category
+    //sending user id, JWT token, and name of category
+    createCategory(user._id, token, {name})
+    .then(data=>{
+        //if any error we pass that error to state
+        if(data.error){
+            setError(true)
+        }else{
+            //if no error we make sure to clean any old error data
+            // then pass true to Success state
+            setError('')
+            setSuccess(true)
+        }
+    })
 
 }
 
 const newCategoryForm =()=>{
-    <form onSubmit={clickSubmit}>
-        <div className="form-group">
-            <label className="text-muted">Name</label>
-            <input type="text" className="form-control" onChange={handleChange} value={name} autoFocus/>
+    return(
+        <form onSubmit={clickSubmit}>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input type="text" className="form-control" required onChange={handleChange} value={name} autoFocus/>
+            </div>
             <button className="btn btn-outline-primary">Create Category</button>
-        </div>
-    </form>
+        </form>
+    )
 }
 
+    const showSuccess =()=>{
+        if(success){
+            return(
+                <h3 className="text-success">{name} has been created!</h3>
+            )
+        }
+    }
+
+    const showError =()=>{
+        if(error){
+            return(
+                <h3 className="text-danger">{name} already exist!</h3>
+            )
+        }
+    }
+
+    const goBack =()=>(
+       <div className="mt-5">
+           <Link to="/admin/dashboard" className="text-warning">Back to Dashboard </Link>
+       </div>
+    )
+
     return(
-        <Layout title="Add a new Category" description={`Welcome ${name}, ready to add a new Category?`}>
+        <Layout title="Add a new Category" description={`Welcome ${user.name}, ready to add a new Category?`}>
             
                 <div className="col-md-8 offset-md-2">
+                    {showSuccess()}
+                    {showError()}
                     {newCategoryForm()}
+                    {goBack()}
                 </div>
             
         </Layout>
---
+++ ecommerce-front/src/admin/apiAdmin.js
@@ -0,0 +1,27 @@
+import { API } from '../config.js'
+    
+    
+    
+   const createCategory = (userId,token,category) =>{
+       return ( fetch(`${API}/category/create/${userId}`, {
+            method: "POST",
+            headers:{
+                Accept: 'application/json',
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            },
+            // passing the object category as json
+            body: JSON.stringify(category)
+        })
+        .then(response => {
+            return response.json()
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+       )
+    }
+
+    export {
+        createCategory,
+    }
