fa1bf0446486aa38def5159afa9a21bc831bdd25
jarredsimon <48073952+jarredsimon@users.noreply.github.com>
2021-03-25 16:24:42 -0500

    working on homepage, currently we have the ability to display all items with sorting by amount sold and created date

--- ecommerce-front/src/core/Home.js
+++ ecommerce-front/src/core/Home.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from './Layout.js'
+import { getProducts } from './apiCore'
 
 const Home = () =>{
+
+    const [productsBySell, setProductsBySell]=useState([])
+    const [productsByArrival, setProductsByArrival]=useState([])
+    const [error, setError]=useState(false)
+
+
+    const loadProductsBySell =()=>{
+        //this will grab the products and order them by SOLD, since we are passing sold in as the arg
+        getProducts('sold').then(data=>{
+            if(data.error){
+                setError(data.error)
+            }else{
+                setProductsBySell(data)
+            }
+        })
+    }
+
+    //this will grab the products and order them by ARRIVAL, since we are passing arrival in as the arg
+    const loadProductsByArrival =()=>{
+        getProducts('createdAt').then(data=>{
+            if(data.error){
+                setError(data.error)
+            }else{
+                setProductsByArrival(data)
+            }
+        })
+    }
+
+    useEffect(()=>{
+        loadProductsByArrival()
+        loadProductsBySell()
+    },[])
+
     
     return(
         <Layout title="Home Page" description="Node React E-commerce App">
-            ...
+            {JSON.stringify(productsByArrival)}
+            <hr/>
+            {JSON.stringify(productsBySell)}
+            
         </Layout>
     )
 }
